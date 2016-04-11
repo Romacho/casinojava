abf4760232777d4926253e57e54242b93e65f9a9
Romacho <javier.romacho@gmail.com>
2016-04-11 21:00:25 +0200

    Subir la primera versión del proyecto: -Pantalla de Loggin -Pantalla de seleccion de juegos (menú principal) -Versión Beta de la ruleta (gira, elige números, informa de las apuestas exactas realizadas en el número 0 y el número 1 con un sistema de colores)

---
+++ src/ruleta/ruleta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ruleta;
+
+/**
+ *
+ * @author Ague
+ */
+public class ruleta {
+    int apuesta_num, apuesta_fichas;
+}
